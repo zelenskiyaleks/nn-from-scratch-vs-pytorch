1bec62efa6d877d1225ce55361dc22cf2b1e7413
Aleksandr Zelenskiy <zelenskiyaleks@gmail.com>
2026-03-18 00:05:51 +0000

    chore: update gitignore and remove checkpoints

--- numpy_nn/.ipynb_checkpoints/activations-checkpoint.py
+++
@@ -1,7 +0,0 @@
-class ReLU:
-    def forward(self, x):
-        self.mask = x > 0
-        return x * self.mask
-
-    def backward(self, grad_output):
-        return grad_output * self.mask
--- numpy_nn/.ipynb_checkpoints/loss-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class MSELoss:
-    def forward(self, y_pred, y_true):
-        self.diff = y_pred - y_true
-        return np.mean(self.diff ** 2)
-
-    def backward(self):
-        return 2 * self.diff / self.diff.shape[0]
